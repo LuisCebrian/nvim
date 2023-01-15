05268f5c3d1803f0b639838e3919abfea6ca69ac
luis <luicebch@gmail.com>
2023-01-15 19:47:22 +0100

    Lazy load lsp-zero.

--- lua/src/lazy.lua
+++ lua/src/lazy.lua
@@ -82,28 +82,6 @@ return require("lazy").setup({
         end
     },
 
-    -- LSP
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
-
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "hrsh7th/cmp-cmdline" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lua" },
-
-            -- Snippets
-            { "L3MON4D3/LuaSnip" }
-        }
-    },
     {
         'j-hui/fidget.nvim',
         config = function() require("fidget").setup() end
