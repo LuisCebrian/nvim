2512aab71ab62409ebbf0910ac90f094d4c8feac
LuisCebrian <luicebch@gmail.com>
2024-01-21 20:55:10 +0100

    Fix auto loading of python virtual environment

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -4,20 +4,20 @@ return {
     event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     config = function()
         require('venv-selector').setup({
-            -- dap_enabled = true,
+            dap_enabled = true,
             name = { "venv", ".env" },
             fd_binary_name = "fdfind"
         })
         -- Load the python environment automatically
         local group = vim.api.nvim_create_augroup("VenvAutoActivation", { clear = true })
 
-        vim.api.nvim_create_autocmd("BufEnter", {
+        vim.api.nvim_create_autocmd({ "LspAttach", "BufEnter" }, {
             desc = "Auto select virtualenv when reading a python file",
             pattern = "*.py",
             group = group,
             callback = function()
                 -- If there is a venv active, dont load it again
-                if require('venv-selector').get_active_venv() == nil and os.getenv("VIRTUAL_ENV") == "" then
+                if require('venv-selector').get_active_venv() == nil and os.getenv("VIRTUAL_ENV") == nil then
                     -- Defer the callback to give some time to the diagnostics to detect the new environment
                     vim.defer_fn(require("venv-selector").retrieve_from_cache, 2000)
                 end
@@ -32,5 +32,7 @@ return {
                 require("venv-selector").deactivate_venv()
             end
         })
+
+        vim.keymap.set('n', '<leader>vs', '<cmd>VenvSelect<cr>', {})
     end
 }
