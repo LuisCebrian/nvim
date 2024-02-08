fe5c5d50277e5bb29c726df563ec0af1f5c718f9
LuisCebrian <luicebch@gmail.com>
2024-02-08 11:12:06 +0100

    Prevent lua_ls from showing warnings in plugin setup.
    - Update plugins
    - Fix Markdown preview plugin not opening window in browser

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,7 +31,16 @@ return {
 
             local lspconfig = require("lspconfig")
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            missing_parameters = false
+                        }
+                    }
+                }
+            })
             lspconfig.pyright.setup({ capabilities = capabilities })
             lspconfig.dockerls.setup({ capabilities = capabilities })
             lspconfig.jsonls.setup({ capabilities = capabilities })
