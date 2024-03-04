bcd5660726d4092274e74fb2fc936242b91c49e0
LuisCebrian <luicebch@gmail.com>
2024-03-04 11:14:10 +0100

    Enable comments in dbt buffers

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,5 +3,8 @@ return {
     event = "BufReadPost",
     config = function()
         require("Comment").setup()
+        -- Enable comments for dbt buffers
+        local ft = require("Comment.ft")
+        ft.set('dbt', '--%s')
     end
 }
