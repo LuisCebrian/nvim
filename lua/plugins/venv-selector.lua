32b354aa89d9fdb3243882ee8c6d14de07a0472d
LuisCebrian <luicebch@gmail.com>
2025-10-02 10:45:14 +0200

    Update plugins
    - Put treesitter parsers explicitly since some of them fail to install
    - Use correct branch of venv-selector

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -2,7 +2,7 @@ return {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-    branch = "regexp",
+    branch = "main",
     config = function()
         require('venv-selector').setup({
             dap_enabled = true,
