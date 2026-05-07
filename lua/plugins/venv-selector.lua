8574a4d2ef2f7a5f39dda336f3c5a0930b4b987a
LuisCebrian <luicebch@gmail.com>
2026-05-07 13:31:35 +0200

    Remove deprecated options from venv-selector config
    - Remove dap_enabled (now auto-detected by the plugin)
    - Remove name option (no longer exists after plugin rewrite)

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -5,8 +5,6 @@ return {
     branch = "main",
     config = function()
         require('venv-selector').setup({
-            dap_enabled = true,
-            name = { "venv", ".env" },
             fd_binary_name = "fdfind"
         })
         vim.keymap.set('n', '<leader>vs', '<cmd>VenvSelect<cr>', {})
